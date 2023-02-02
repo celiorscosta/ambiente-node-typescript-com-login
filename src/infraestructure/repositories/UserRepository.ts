caac2dc63f9f96810b034e341e607a1eec0b341b
celiorscosta <celiorscosta@hotmail.com>
2023-02-02 17:22:15 -0300

    try catch

--- src/infraestructure/repositories/UserRepository.ts
+++ src/infraestructure/repositories/UserRepository.ts
@@ -1,23 +1,72 @@
+import { PrismaClient } from "@prisma/client";
 import { User } from "src/domain/entities/User";
 import { IUserRepository } from "src/domain/repositories/IUserRepository";
 
 export class UserRepository implements IUserRepository {
-    findByEmail(email: string): Promise<User> {
-        throw new Error("Method not implemented.");
+    private prisma = new PrismaClient();
+
+    async create(data: User): Promise<User> {
+        try {
+            const createdUser = await this.prisma.user.create({
+                data: data,
+            });
+            return createdUser;
+        } catch (error) {
+            throw new Error(`Failed to create user: ${error.message}`);
+        }
     }
-    create(data: User): Promise<User> {
-        throw new Error("Method not implemented.");
+
+    async findByEmail(email: string): Promise<User> {
+        try {
+            const user = await this.prisma.user.findOne({
+                where: { email },
+            });
+            return user || null;
+        } catch (error) {
+            throw new Error(`Failed to retrieve user with email ${email}: ${error.message}`);
+        }
     }
-    findAll(): Promise<User[]> {
-        throw new Error("Method not implemented.");
+
+    async findAll(): Promise<User[]> {
+        try {
+            const users = await this.prisma.user.findMany();
+            return users;
+        } catch (error) {
+            throw new Error(`Failed to retrieve users: ${error.message}`);
+        }
     }
-    findById(id: string): Promise<User> {
-        throw new Error("Method not implemented.");
+
+    async findById(id: string): Promise<User> {
+        try {
+            const user = await this.prisma.user.findOne({
+                where: { id },
+            });
+            return user || null;
+        } catch (error) {
+            throw new Error(`Failed to retrieve user with id ${id}: ${error.message}`);
+        }
     }
-    update(id: string, data: User): Promise<void> {
-        throw new Error("Method not implemented.");
+
+    async update(id: string, data: User): Promise<void> {
+        try {
+            const updatedUser = await this.prisma.user.update({
+                where: { id: id },
+                data: data,
+            });
+            return updatedUser;
+        } catch (error) {
+            throw new Error(`Failed to update user with id ${id}: ${error.message}`);
+        }
     }
-    delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
+
+    async delete(id: string): Promise<void> {
+        try {
+            await this.prisma.user.delete({
+                where: { id },
+            });
+        } catch (error) {
+            throw new Error(`Failed to delete user with id ${id}: ${error.message}`);
+        }
     }
+
 }
