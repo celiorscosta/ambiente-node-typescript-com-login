5b0f91f918d0b264b6ba40b8a28aba6eb43e0916
celiorscosta <celiorscosta@hotmail.com>
2023-02-03 09:50:29 -0300

    adicionando testes, porem, ainda não passam.

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,10 +1,10 @@
 export class User {
-    private _id: string;
+    private _id: string | null;
     private _name: string;
     private _email: string;
     private _password: string;
 
-    constructor(id: string, name: string, email: string, password: string) {
+    constructor(id: string | null, name: string, email: string, password: string) {
         this._id = id;
         this._name = name;
         this._email = email;
---
+++ tests/infraestructure/repositories/UserRepository.test.ts
@@ -0,0 +1,30 @@
+import { UserRepository } from "../../../src/infraestructure/repositories/UserRepository";
+import { User } from "../../../src/domain/entities/User";
+
+const makeSut = (): UserRepository => {
+    return new UserRepository();
+};
+
+describe("UserRepository", () => {
+    it("should be able to create a user", async () => {
+        const sut = makeSut();
+        const user = new User(null, "Lucas", "email@email.com", "password");
+
+        const createdUser = await sut.create(user);
+        expect(createdUser).toEqual(user);
+    });
+
+    it("should be able to find a user by email", async () => {
+        const sut = makeSut();
+        const user = new User(null, "Lucas", "email@email.com", "password");
+        const createdUser = await sut.create(user);
+        const foundUser = await sut.findByEmail("email@email.com");
+        expect(foundUser).toEqual(createdUser);
+    });
+
+    it("should return null if no user was found with the email", async () => {
+        const sut = makeSut();
+        const user = await sut.findByEmail("invalid_email@email.com");
+        expect(user).toBeNull();
+    });
+});
